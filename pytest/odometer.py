5b7eab6eff9118b504940a91f5d364b2c2f32ad1
priyansi <priyanhiman@gmail.com>
2020-12-18 11:36:47 +0530

    base files for pytest

---
+++ pytest/odometer.py
@@ -0,0 +1,56 @@
+class Odometer:
+    def is_ascending(n):
+        if n < 10:
+            return True
+        if n % 10 <= (n // 10) % 10:
+            return False
+        return Odometer.is_ascending(n // 10)
+
+    def __init__(self, size):
+        self.readings = []
+        if size not in range(2, 9):
+            self.size = 0
+            return 
+        for n in range(10 ** (size - 1), 10 ** size):
+            if Odometer.is_ascending(n):
+                self.readings.append(n)
+        self.SIZE = size
+        self.LENGTH = len(self.readings)
+        self.position = 0
+        return
+    
+    def __DEBUG__(self):
+        print(f"Size = {self.SIZE}, First = {self.readings[0]}", end = " ")
+        print(f"Last = {self.readings[-1]}", end = " ")
+        print(f"Current = {self.readings[self.position]}", end = " ")
+        print(f"Position = {self.position}")
+
+    def next_reading(self, step = 1):
+        self.position += step
+        self.position %= self.LENGTH
+        return
+
+    def prev_reading(self, step = 1):
+        self.position -= step
+        self.position %= self.LENGTH
+        return
+
+    def diff(self, o):
+        if self.SIZE != o.SIZE:
+            return -1
+        togo = o.position - self.position
+        if togo < 0:
+            togo += self.LENGTH
+        return togo
+
+o = Odometer(3)
+o.next_reading(4)
+o.__DEBUG__()
+o.prev_reading(6)
+o.__DEBUG__()
+p = Odometer(3)
+p.__DEBUG__()
+print(o.diff(p))
+print(p.diff(o))
+
+
---
+++ pytest/test_odometer.py
@@ -0,0 +1,14 @@
+def test_is_ascending():
+    pass
+
+
+def test_next_reading():
+    pass
+
+
+def test_previous_reading():
+    pass
+
+
+def test_diff():
+    pass
