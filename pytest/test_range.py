dfb2c0d3643adef1be3e70a75e865a3ff1e9ea46
Aaryashivani <sundropdiamond@gmail.com>
2020-12-20 11:13:55 +0530

    Created basic file for range tests

---
+++ pytest/test_range.py
@@ -0,0 +1,21 @@
+from range import Range
+
+
+def test_contains():
+    pass
+
+
+def test_overlaps():
+    pass
+
+
+def test_disjoint():
+    pass
+
+
+def test_touching():
+    pass
+
+
+def test_less_than():
+    pass
