33a5926460f228ea47f76ccd1e9c3285d7df4f58
Kushal Beniwal <kbeniwal2305@gmail.com>
2020-12-22 16:43:18 +0530

    added testRange class

---
+++ unittest/test_range_func.py
@@ -0,0 +1,22 @@
+import unittest
+from range_func import Range
+
+class TestRangeMethods(unittest.TestCase):
+
+    def test_contains(self):
+        pass
+
+    def test_overlaps(self):
+       pass
+
+    def test_disjoint(self):
+        pass    
+
+    def test_touching(self):
+        pass
+
+    def test_less_than(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
