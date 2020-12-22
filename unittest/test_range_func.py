29104152ed0be75dccd3a739b0579f8f31c82b58
Kushal Beniwal <kbeniwal2305@gmail.com>
2020-12-22 17:19:11 +0530

    added tests for range methods

--- unittest/test_range_func.py
+++ unittest/test_range_func.py
@@ -4,19 +4,41 @@ from range_func import Range
 class TestRangeMethods(unittest.TestCase):
 
     def test_contains(self):
-        pass
+        self.assertTrue(Range(4).contains(Range(0, 4)))
+        self.assertTrue(Range(5).contains(Range(4)))
+        self.assertTrue(Range(1, 4).contains(Range(3, 4)))
+        self.assertFalse(Range(4).contains(Range(5)))
+        self.assertFalse(Range(1, 4).contains(Range(3, 5)))
+        self.assertFalse(Range(1, 2).contains(Range(4, 5)))
+        self.assertFalse(Range(4, 5).contains(Range(1, 2)))
 
     def test_overlaps(self):
-       pass
+        self.assertTrue(Range(1, 3).overlaps(Range(2, 3)))
+        self.assertTrue(Range(2, 3).overlaps(Range(1, 3)))
+        self.assertFalse(Range(1, 5).overlaps(Range(6, 7)))
+        self.assertFalse(Range(6, 7).overlaps(Range(1, 5)))
 
     def test_disjoint(self):
-        pass    
+        self.assertTrue(Range(1, 2).disjoint(Range(3, 4)))
+        self.assertTrue(Range(3, 4).disjoint(Range(1, 2)))
+        self.assertFalse(Range(1, 3).disjoint(Range(2, 3)))
+        self.assertFalse(Range(2, 3).disjoint(Range(1, 3)))
 
     def test_touching(self):
-        pass
+        self.assertTrue(Range(1, 2).touching(Range(2, 7)))
+        self.assertTrue(Range(2, 7).touching(Range(1, 2)))
+        self.assertFalse(Range(1, 5).touching(Range(4, 7)))
+        self.assertFalse(Range(1, 2).touching(Range(4, 7)))
+        self.assertFalse(Range(4, 7).touching(Range(1, 2)))
 
     def test_less_than(self):
-        pass
+        self.assertTrue(Range(1, 3).less_than(Range(3, 10)))
+        self.assertTrue(Range(1).less_than(Range(2)))
+        self.assertFalse(Range(4, 5).less_than(Range(1, 2)))
+        self.assertFalse(Range(2).less_than(Range(1)))
+        self.assertFalse(Range(1).less_than(Range(1)))
+        self.assertFalse(Range(1, 4).less_than(Range(2, 5)))
+        self.assertFalse(Range(2, 6).less_than(Range(1, 4)))
 
 if __name__ == "__main__":
     unittest.main()
