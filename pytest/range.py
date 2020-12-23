61eb61822a4596ccb5af3a0d8601d02f3753f7ce
priyansi <priyanhiman@gmail.com>
2020-12-23 08:54:06 +0530

    added range class structure, tests might be broken

--- pytest/range.py
+++ pytest/range.py
@@ -5,13 +5,17 @@ class Range:
         Sets starts to 0 if only one argument provided else
         sets start and end according to the arguments passed.
         """
+        if(len(args) < 1 or len(args) > 2):
+            raise ValueError
         self.start = 0 if len(args) == 1 else args[0]
         self.end = args[0] if len(args) == 1 else args[1]
         return
 
-    def __DEBUG__(self):
-        print(f"Start = {self.start}, End = {self.end}")
-        return
+    def __repr__(self):
+        return f"[{self.start}, {self.end})"
+
+    def __len__(self):
+        return self.end - self.start
 
     def contains(self, sub) -> bool:
         """
@@ -20,21 +24,21 @@ class Range:
         """
         return self.start <= sub.start and self.end >= sub.end
 
+    def disjoint(self, sub) -> bool:
+        """
+        If two ranges don't overlap, then they are disjoint.
+        """
+        return sub.start < self.end or self.start < sub.end
+
     def overlaps(self, sub) -> bool:
         """
         Finds the smaller of the two starting points and checks if
         the end point of the one that started eariler finishes before
         or after the range that started after.
         """
-        return sub.start < self.end if self.start < sub.start else self.start < sub.end
+        return not self.disjoint(sub)
 
-    def disjoint(self, sub) -> bool:
-        """
-        If two ranges don't overlap, then they are disjoint.
-        """
-        return not self.overlaps(sub)
-
-    def touching(self, sub) -> bool:
+    def is_touching(self, sub) -> bool:
         """
         Finds the smaller of the two starting points and checks if
         the end point of the one that started eariler is equal to
@@ -42,24 +46,68 @@ class Range:
         """
         return sub.start == self.end if self.start < sub.start else self.start == sub.end
 
-    def less_than(self, sub) -> bool:
+    def is_less_than(self, sub) -> bool:
         """
-        Returns True if the length of the super range is
-        less than the length of the sub range
+        If both start points are equal, returns True if the super range 
+        ends before the sub range else returns True if the super range
         """
-        return (self.end - self.start) < (sub.end - sub.start) if self.start == sub.start else self.start < sub.start
+        return self.end < sub.end if self.start == sub.start else self.start < sub.start
+
+    def is_more_than(self, sub) -> bool:
+        """
+        If both start points are equal, returns True if the super range ends before the sub range
+        """
+        return self.end > sub.end if self.start == sub.start else self.start > sub.start
+
+    def is_equal_to(self, sub) -> bool:
+        """
+        Returns True if the starting and ending points of both
+        ranges are the same.
+        """
+        return self.start == sub.start and self.end == sub.end
+
+    def stretch_left(self, left=1):
+        self.start -= left
+
+    def stretch_right(self, right=1):
+        self.end += right
+
+    def stretch(self, left=1, right=1):
+        self.start -= left
+        self.end += right
+
+    def squeeze(self, n=1):
+        pass
+
+    def shift(self, n):
+        pass
+
+    def shift_left(self, n):
+        pass
+
+    def shift_right(self, n):
+        pass
+
+    def classify(self):
+        pass
+
+    def merge(self, sub):
+        return Range(sorted(self.start, self.end, sub.start, sub.end)[1:3])
+
+    def common(self):
+        pass
 
 
 if __name__ == "__main__":
     a = Range(8)
     b = Range(4, 9)
-    a.__DEBUG__()
-    b.__DEBUG__()
+    print(a)
+    print(b)
     print(a.contains(b))
     print(b.contains(a))
     print(a.overlaps(b))
     print(b.overlaps(a))
     print(a.disjoint(b))
     print(b.disjoint(a))
-    print(a.touching(b))
-    print(b.less_than(a))
+    print(a.is_touching(b))
+    print(b.is_less_than(a))
