9c9a249e9f05c937107ae6073d53d942ccbed1e0
pratyusha-05 <76260882+pratyusha-05@users.noreply.github.com>
2020-12-25 09:22:19 +0530

    Update test_odometer.py

--- nose/test_odometer.py
+++ nose/test_odometer.py
@@ -1,18 +1,18 @@
-import odometer
+from odometer import Odometer
 
 class TestOdometer:
     def isAscending(self):
-        assert odometer.Odometer.is_ascending(22) == False
-        assert odometer.Odometer.is_ascending(312) == False
-        assert odometer.Odometer.is_ascending(123) == True
-        assert odometer.Odometer.is_ascending(90) == False
+        assert Odometer(2).is_ascending(22) == False
+        assert Odometer(3).is_ascending(312) == False
+        assert Odometer(3).is_ascending(123) == True
+        assert Odometer(2).is_ascending(90) == False
 
     def nextReading(self):
-        assert odometer.Odometer.next_reading(89) == 12
-        assert odometer.Odometer.next_reading(129) == 134
+        assert Odometer(2).next_reading(89) == 12
+        assert Odometer(3).next_reading(129) == 134
 
     def prevReading(self):
-        assert odometer.Odometer.prev_reading(123) == 789
-        assert odometer.Odometer.next_reading(23) == 19
+        assert Odometer(3).prev_reading(123) == 789
+        assert Odometer(2).next_reading(23) == 19
 
 
